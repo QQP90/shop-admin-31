a42bbca9c534ab6bc3f3e2f7a5b77f0bcbc15ef9
lilongqiang <1751014403@qq.com>
2019-06-08 21:52:46 +0800

    添加了登录页面和admin的初始页面

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -15,12 +15,24 @@
           <i class="el-icon-menu"></i>
           <span>购物商城</span>
         </template>
-
         <el-menu-item index="1-1">
           <template slot="title">商品管理</template>
         </el-menu-item>
         <el-menu-item index="1-2">
           <template slot="title">栏目管理</template>
+        </el-menu-item>
+      </el-submenu>
+       <el-submenu index="2">
+          <!-- slot是模板中的数据 这里不用-->
+        <template slot="title">
+          <i class="el-icon-menu"></i>
+          <span>购物</span>
+        </template>
+        <el-menu-item index="2-1">
+          <template slot="title">已购</template>
+        </el-menu-item>
+        <el-menu-item index="2-2">
+          <template slot="title">未购</template>
         </el-menu-item>
       </el-submenu>
     </el-menu>
